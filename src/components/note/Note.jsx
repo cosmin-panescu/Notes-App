014a7c7f1ebe1547867308fb9bd4d0befb920b7b
Panescu Cosmin <1pane.cosmin@gmail.com>
2022-10-21 23:09:07 +0300

    editable notes

--- src/components/note/Note.jsx
+++ src/components/note/Note.jsx
@@ -5,7 +5,7 @@ import { TiDeleteOutline } from 'react-icons/ti';
 const Note = ({ id, text, date, handleDeleteNote }) => {
     return (
         <div className='note'>
-            <span>{text}</span>
+            <span contentEditable="true">{text}</span>
             <div className='note-footer'>
                 <small>{date}</small>
                 <TiDeleteOutline
